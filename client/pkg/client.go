de0351bd1cbfe453c7446a974cb7f7035e5d14da
fatmaebrahim <fatmaebrahim3131@gmail.com>
2024-09-21 02:36:30 +0300

    chore: added logging into a file

--- client/pkg/client.go
+++ client/pkg/client.go
@@ -1,42 +1,47 @@
 // This package implements a HTTP client in Go that consumes the datetime server.
 // It supports two content types: plain text and JSON.
 // It has two public functions: Client and Inputhandler.
-
 package client
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
-	"time"
-	"flag"
 	"os"
+	"time"
 
 	"github.com/cenkalti/backoff/v4"
+)
+
+// Logger is used to log messages to logs.log file.
+var (
+	outfile, _ = os.Create("logs.log")
+	logger     = log.New(outfile, "", 0)
 )
 
 // Client sends a request to the datetime server with certain content type (plain text or json)
 // and print the output to the console.
 // and returns the response statuscode, the response body and any countered error.
 func Client(w io.Writer, url, contenttype string) (int, []byte, error) {
-	// host := os.Getenv("SERVER_HOST")
-	// port := os.Getenv("SERVER_PORT")
-	// url := fmt.Sprintf("http://%s:%s/datetime", host, port)
-
 	var response *http.Response
 	connection := func() error {
 		c := http.Client{Timeout: time.Duration(1) * time.Second}
 		request, err := http.NewRequest("GET", url, nil)
 		if err != nil {
+			logger.Println("Failed to create request", err)
 			return err
 		}
 		request.Header.Add("Content-Type", contenttype)
 		response, err = c.Do(request)
-
+		logger.Println("Sending request to server")
 		if err != nil {
+			logger.Println("Failed to send request", err)
 			return err
 		}
+		logger.Println("Request sent successfully")
 		return nil
 	}
 
@@ -44,8 +49,10 @@ func Client(w io.Writer, url, contenttype string) (int, []byte, error) {
 	expBackoff.MaxElapsedTime = 10 * time.Second
 	err := backoff.Retry(connection, expBackoff)
 	if err != nil {
+		logger.Println("Failed to connect to server", err)
 		return http.StatusServiceUnavailable, nil, fmt.Errorf("error in server connection")
 	}
+	logger.Println("Server returned status code", response.StatusCode)
 	if response.StatusCode != http.StatusOK {
 		return response.StatusCode, nil, fmt.Errorf("status code not OK")
 	}
@@ -53,6 +60,7 @@ func Client(w io.Writer, url, contenttype string) (int, []byte, error) {
 	defer response.Body.Close()
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
+		logger.Println("Failed to read response body", err)
 		return response.StatusCode, body, err
 	}
 
@@ -60,11 +68,13 @@ func Client(w io.Writer, url, contenttype string) (int, []byte, error) {
 	if contenttype == "application/json" {
 		data, err = jsontype(body)
 		if err != nil {
+			logger.Println("Failed to read response body", err)
 			return response.StatusCode, body, err
 		}
 	}
 
 	fmt.Fprintln(w, data)
+	logger.Println("Response recieved successfully")
 	return response.StatusCode, body, nil
 }
 
@@ -72,23 +82,25 @@ func Client(w io.Writer, url, contenttype string) (int, []byte, error) {
 // if no flags are provided, it uses the environment variables (SERVER_HOST , SERVER_PORT)
 // if no environment variables are provided, it uses default values
 // then returns the url and content type
-func Inputhandler() (string,  string) {
+func Inputhandler() (string, string) {
 	port := ""
 	flag.StringVar(&port, "p", "", "port number")
 	if port == "" {
 		port = os.Getenv("SERVER_PORT")
 		if port == "" {
-			port="8080"
+			port = "8080"
 		}
 	}
+
 	host := ""
 	flag.StringVar(&host, "h", "", "host name")
 	if host == "" {
 		host = os.Getenv("SERVER_HOST")
 		if host == "" {
-			host="localhost"
+			host = "localhost"
 		}
 	}
+
 	contenttype := ""
 	flag.StringVar(&contenttype, "c", "application/json", "content type")
 	flag.Parse()
@@ -96,7 +108,8 @@ func Inputhandler() (string,  string) {
 	if url == "" {
 		url = "http://" + host + ":" + port + "/datetime"
 	}
-	return url,contenttype
+	logger.Println("Requested URL:", url)
+	return url, contenttype
 }
 
 func texttype(body []byte) string {
@@ -110,4 +123,3 @@ func jsontype(body []byte) (string, error) {
 	}
 	return result["datetime"].(string), nil
 }
-
